56295784abbae5c79a3804e1d7d1a384c90bddd1
Griger5 <griger04@gmail.com>
2025-08-19 19:34:10 +0200

    add: include rttg headers, update dummy source for testing

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,18 @@
 #include "cli-brary/test.hpp"
 
+#include "rttg/rttg.h"
+
 #include <iostream>
+#include <string>
 
 void test() {
-    std::cout << "Hello!" << std::endl;
+    std::cout << "Hello!\n";
+
+    std::tuple<int, float, std::string> tuple{ 5, 3.0, "str"};
+    
+    for (std::size_t idx{}; idx < std::tuple_size_v<decltype(tuple)>; ++idx) {
+        rttg::visit([](auto &&tupleValue) { std::cout << tupleValue << " "; }, rttg::get(tuple, idx));
+    }
+
+    std::cout << std::endl;
 }
