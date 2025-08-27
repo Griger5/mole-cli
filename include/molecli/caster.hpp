5268ff04693f6520adef28f4291ab879a6df5348
Griger5 <griger04@gmail.com>
2025-08-27 19:53:41 +0200

    add: cast implementation for floating point numbers

--- include/molecli/caster.hpp
+++ include/molecli/caster.hpp
@@ -3,11 +3,12 @@
 
 #include <optional>
 #include <string>
+#include <algorithm>
 
 namespace molecli::detail {
 
 template <typename T>
-bool cast(std::string &&token, void *output) {return false;}
+bool cast(std::string &&token, void *output) { return false; }
 
 template <>
 bool cast<bool>(std::string &&token, void *output) {
@@ -30,7 +31,7 @@ template <>
 bool cast<int>(std::string &&token, void *output) {
     std::string copy = token;
 
-    if (copy.at(0) == '-') {
+    if (token.at(0) == '-') {
         copy.erase(0);
     }
 
@@ -41,6 +42,48 @@ bool cast<int>(std::string &&token, void *output) {
     }
     else {
         return false;
+    }
+}
+
+template <>
+bool cast<float>(std::string &&token, void *output) {
+    std::string copy = token;
+
+    if (token.at(0) == '-') {
+        copy.erase(0);
+    }
+    
+    if (copy.find_first_not_of("0123456789.") != std::string::npos) {
+        return false;
+    }
+    else if (std::count(token.begin(), token.end(), '.') > 1) {
+        return false;
+    }
+    else {
+        float *output_float = static_cast<float *>(output);
+        *output_float = std::stof(token);
+        return true;
+    }
+}
+
+template <>
+bool cast<double>(std::string &&token, void *output) {
+    std::string copy = token;
+
+    if (token.at(0) == '-') {
+        copy.erase(0);
+    }
+    
+    if (copy.find_first_not_of("0123456789.") != std::string::npos) {
+        return false;
+    }
+    else if (std::count(token.begin(), token.end(), '.') > 1) {
+        return false;
+    }
+    else {
+        double *output_double = static_cast<double *>(output);
+        *output_double = std::stof(token);
+        return true;
     }
 }
 
