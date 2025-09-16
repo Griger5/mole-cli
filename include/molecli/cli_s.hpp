fb0a4b457d93b67e27fd0f7e63f58f44ad88fed6
Griger5 <griger04@gmail.com>
2025-09-16 15:52:16 +0200

    update: split CLI class into two classes

--- include/molecli/cli_s.hpp
+++ include/molecli/cli_s.hpp
@@ -1,20 +1,7 @@
-#ifndef MOLECLI_CLI_HPP
-#define MOLECLI_CLI_HPP
+#ifndef MOLECLI_CLI_S_HPP
+#define MOLECLI_CLI_S_HPP
 
-#include "command.hpp"
-#include "command_s.hpp"
-#include "tokenize.hpp"
-#include "caster.hpp"
-#include "help_message.hpp"
-#include "static_vars.hpp"
-
-#include "external/isocline/include/isocline.h"
-
-#include <map>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <cxxabi.h>
+#include "cli.hpp"
 
 namespace molecli {
 
@@ -22,74 +9,51 @@ using Args = std::vector<void *>;
 using Caster = std::function<bool(std::string &&, void *)>;
 using DeallocFunc = std::function<void(Args &, const std::size_t &)>;
 
-class CLI {
-protected:
-    const std::string prompt;
-    std::map<std::string, detail::Command> commands;
-    std::map<std::string, detail::HelpMessage> help_messages;
+template <typename... StaticTypes>
+class CLI_s final : public CLI {
+public:
+    using StaticVarsT = detail::StaticVars<StaticTypes...>;
 
-    template <std::size_t Idx, typename First = void, typename... Rest>
-    void add_command_impl(Args &arg_vec, std::vector<Caster> &caster_vec, std::vector<DeallocFunc> &dealloc_vec, std::vector<std::string> &type_names_vec) {
-        if constexpr (Idx > 0) { 
-            arg_vec.push_back(new First);
-            caster_vec.push_back(detail::cast<First>);
-            dealloc_vec.push_back([](std::vector<void*> &v, std::size_t idx) {delete static_cast<First *>(v[idx]);});
-
-            int status = -1;
-            char *name = abi::__cxa_demangle(typeid(First).name(), 0, 0, &status);
-            std::string name_str{name};
-            if (name_str == "std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >") {
-                type_names_vec.push_back("std::string");
-            }
-            else {
-                type_names_vec.push_back(name_str);
-            }
-            free(name);
-            
-            add_command_impl<Idx - 1, Rest...>(arg_vec, caster_vec, dealloc_vec, type_names_vec);
-        }
-    }
-
-    template <std::size_t Idx, typename... ArgTypes>
-    void vector_set_tuple(std::tuple<ArgTypes...> &t, const std::vector<void*> &v) {
-        if constexpr (Idx != 0) {
-            std::get<Idx-1>(t) = *static_cast<std::tuple_element_t<Idx-1, std::tuple<ArgTypes...>> *>(v[Idx-1]);
-            vector_set_tuple<Idx-1>(t, v);
-        }
-    }
+private:
+    std::map<std::string, detail::Command_s<StaticTypes...>> static_commands;
+    std::shared_ptr<StaticVarsT> static_vars;
 
 public:
-    CLI() : prompt{""} {}
+    CLI_s() : CLI{}, static_vars{std::make_shared<StaticVarsT>()} {}
 
-    CLI(const std::string &p) : prompt{p} {}
+    CLI_s(const std::string &p) : CLI{p}, static_vars{std::make_shared<StaticVarsT>()} {}
 
-    virtual ~CLI() {
-        for (auto [name, cmd] : this->commands) {
+    ~CLI_s() {
+        for (auto [name, cmd] : this->static_commands) {
             cmd.dealloc();
         }
     }
 
     template <typename ReturnType, typename... ArgTypes>
-    void add_command(std::string &&command_name, std::string &&description, std::function<ReturnType(ArgTypes...)> func) {
+    void add_command_s(std::string &&command_name, std::string &&description, std::function<ReturnType(std::shared_ptr<StaticVarsT>, ArgTypes...)> func) {
         Args arg_vec;
         std::vector<Caster> caster_vec;
         std::vector<DeallocFunc> dealloc_vec;
         std::vector<std::string> type_names_vec;
+
+        type_names_vec.push_back("StaticVars");
     
         this->add_command_impl<sizeof...(ArgTypes), ArgTypes...>(arg_vec, caster_vec, dealloc_vec, type_names_vec);
 
-        std::function<void(const Args &)> func_wrapper = [=](const Args &v) {
+        std::function<void(std::shared_ptr<StaticVarsT>, const Args &)> func_wrapper = [=](std::shared_ptr<StaticVarsT> s, const Args &v) {
             std::tuple<ArgTypes...> t;
+
             this->vector_set_tuple<sizeof...(ArgTypes)>(t, v);
 
-            std::apply(func, t);
+            auto t2 = std::tuple_cat(std::make_tuple(s), t);
+            std::apply(func, t2);
         };
 
         this->help_messages[command_name] = detail::HelpMessage{std::move(command_name), std::move(description), std::move(type_names_vec)};
-        this->commands[command_name] = detail::Command{std::move(func_wrapper), std::move(arg_vec), std::move(caster_vec), std::move(dealloc_vec), std::move(type_names_vec)};
+        this->static_commands[command_name] = detail::Command_s{std::move(func_wrapper), std::move(arg_vec), std::move(caster_vec), std::move(dealloc_vec), std::move(type_names_vec)};
     }
 
-    virtual void run_loop(std::ostream &stream = std::cout) {
+    void run_loop(std::ostream &stream = std::cout) override {
         char *temp;
         ic_set_history(NULL, -1);
 
@@ -109,6 +73,24 @@ public:
                 switch(status.code) {
                     case detail::Command::NO_ERROR:
                         this->commands[command_name].execute();
+                        break;
+                    case detail::Command::INSUFFICIENT_COUNT:
+                        stream << "Warning: Insufficient number of arguments. Expected: " << status.arg_count << ", received: " << status.error_idx << '\n';
+                        break;
+                    case detail::Command::TOO_MANY_ARGS:
+                        stream << "Warning: Too many arguments. Expected: " << status.arg_count << ", received: " << status.error_idx << '\n';
+                        break;
+                    case detail::Command::WRONG_TYPE:
+                        stream << "Warning: Wrong type of argument #" << status.error_idx + 1 << ". Argument type should be: " << status.type_name << '\n';
+                        break;
+                }
+            }
+            else if (this->static_commands.find(command_name) != this->static_commands.end()) {
+                detail::Command::Status status = this->static_commands[command_name].load_arguments(arguments);
+
+                switch(status.code) {
+                    case detail::Command::NO_ERROR:
+                        this->static_commands[command_name].execute(this->static_vars);
                         break;
                     case detail::Command::INSUFFICIENT_COUNT:
                         stream << "Warning: Insufficient number of arguments. Expected: " << status.arg_count << ", received: " << status.error_idx << '\n';
@@ -143,4 +125,4 @@ public:
 
 } // molecli
 
-#endif // MOLECLI_CLI_HPP
+#endif // MOLECLI_CLI_S_HPP
